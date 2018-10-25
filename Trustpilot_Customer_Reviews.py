08a4d5f2b2f208f8016b3bff9423d7eddb5d327e
biglala89 <chowli1103@gmail.com>
2018-10-24 19:15:22 -0700

    object-oriented approach

--- Trustpilot_Customer_Reviews.py
+++ Trustpilot_Customer_Reviews.py
@@ -5,80 +5,117 @@
 
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
-from pandas import DataFrame
-from time import strftime
 import re, os, time, sqlite3
 
-os.chdir(file_path)
+# Set up paths and environment
 link = 'https://www.trustpilot.com/review/www.bhphotovideo.com?page='
-database = Filepath/sqlitestudio-3.1.1/SQLiteStudio/Customer_Reviews
+db_name = 'Customer_Reviews'
+database = os.getcwd()+'/{}.db'.format(db_name)
+number_of_pages = 10 # Enter an arbitrary number
 
-cols = ['User', 'Rating', 'Time', 'Title', 'Review']
-tmp = DataFrame(columns=cols)
-df = DataFrame(columns=cols)
+# SQL statements
+drop_reviews = """DROP TABLE IF EXISTS Reviews;"""
+drop_users = """DROP TABLE IF EXISTS Users;"""
+create_reviews = """CREATE TABLE IF NOT EXISTS Reviews (
+							ID INTEGER PRIMARY KEY AUTOINCREMENT,
+							Rating INT,
+							Comment TEXT,
+							Timestamp_ [CURRENT_TIMESTAMP]
+					);"""
+create_users = """CREATE TABLE IF NOT EXISTS Users (
+							ID INTEGER PRIMARY KEY AUTOINCREMENT,
+							Name TEXT
+					);"""
 
-# Populate tables in sqlite
-def table_populate(conn, c, name, rating, time, title, comment):
-    c.execute("INSERT INTO Users VALUES (?, ?, ?)", (name, rating, time))
-    c.execute("INSERT INTO Reviews VALUES (?, ?, ?)", (title, comment, time))
+class Scrape_User_Reviews(object):
+	def __init__(self, link, database, number_of_pages):
+		self.link = link
+		self.database = database
+		self.number_of_pages = number_of_pages
 
-# Get data and create dataframe to store the data
-def get_data(link):  
-    for c in range(5):
-        try:
-            sc = urlopen(link)
-            print "%s attempt" % str(c+1)
-            break
-        except Exception as e:
-            print e
-            continue
-    
-    source = sc.read()#.decode(encoding='utf-8')
-    
-    soup = BeautifulSoup(source, 'lxml')
-    
-    names = soup.find_all('a', attrs={'class': 'user-review-name-link'})
-    name_list = [name.text.strip() for name in names]
-    
-    rating_list = re.findall('<.*?star-rating count-(\d+) size-medium.*?>', source, flags=re.I | re.S)
-    
-    timestamps = soup.find_all('time', attrs={'class': 'ndate'})
-    time_list = [stamp["title"] for stamp in timestamps]
-    
-    summaries = soup.find_all('a', attrs={'class': 'review-title-link'})
-    summary_list = [summary.text for summary in summaries]
-    
-    reviews = soup.find_all('div', attrs={'class': 'review-body'})
-    review_list = [review.text.strip() for review in reviews]
-    
-    conn = sqlite3.connect(database)
-    c = conn.cursor()
-    
-    for i in xrange(len(names)):
-        tmp.loc[i] = [name_list[i], rating_list[i], time_list[i], summary_list[i], review_list[i]]
-        table_populate(conn, c, name_list[i], rating_list[i], time_list[i], summary_list[i], review_list[i])
-        try:
-            conn.commit()
-        except Exception as e:
-            print e
-            conn.rollback()
-            print "Rolled back"           
-    conn.close()
-    
+	# Create connection
+	def Create_DB_and_Tables(self, database):
+		try:
+			conn = sqlite3.connect(database)
+			print sqlite3.version
+			self.Execute_SQL(conn, drop_reviews)
+			self.Execute_SQL(conn, drop_users)
+			self.Execute_SQL(conn, create_reviews)
+			self.Execute_SQL(conn, create_users)
+		except Exception as e:
+			print e
+		finally:
+			conn.close()
+
+	# Create tables in database
+	def Execute_SQL(self, conn, query):
+		try:
+			c = conn.cursor()
+			c.execute(query)
+		except Exception as e:
+			print e
+
+	# Populate tables function
+	def Table_Populate(self, conn, name, rating, time, comment):
+		cur = conn.cursor()
+		cur.execute("INSERT INTO Users (Name) VALUES (?)", [name])
+		cur.execute("INSERT INTO Reviews (Rating, Comment, Timestamp_) VALUES (?, ?, ?)", [rating, comment, time])
+
+	# Get data and populate tables in database
+	def Parse_Data(self, webpage):  
+		for i in xrange(3):
+			try:
+				sc = urlopen(webpage)
+				time.sleep(0.5)
+				break
+			except Exception as e:
+				print "%s attempt failed, giving it another try" % str(i+1)
+				with open('error.log', 'a') as f:
+					f.write(str(e)+'\n')
+				continue
+		
+		source = sc.read()#.decode(encoding='utf-8')
+		soup = BeautifulSoup(source, 'lxml')
+		
+		names = soup.find_all('h3', attrs={'class': 'consumer-info__details__name'})
+		name_list = [name.text.strip() for name in names]
+		
+		rating_list = re.findall('<.*?star-rating star-rating-(\d+) star-rating--medium.*?>', source, flags=re.I | re.S)
+		
+		timestamps = soup.find_all('time', attrs={'class': 'ndate'})
+		time_list = [stamp["title"] for stamp in timestamps]
+		
+		reviews = soup.find_all('p', attrs={'class': 'review-info__body__text'})
+		review_list = [review.text.strip() for review in reviews]
+		
+		try:
+			conn = sqlite3.connect(database)
+			for i in xrange(len(names)):
+				self.Table_Populate(conn, name_list[i], rating_list[i], time_list[i], review_list[i])
+			conn.commit()
+		except Exception as e:
+			print e
+			conn.rollback()
+			print "Roll back"
+		finally:
+			conn.close()
+		
+	# Main function
+	def Run(self):
+		for num in xrange(1, number_of_pages+1):
+			page = link+str(num)
+			print "Downloading %s" % page
+			try:
+				self.Parse_Data(page)
+			except Exception as e:
+				with open('error.log', 'a') as f:
+					f.write(str(page)+'\n'+str(e)+'\n')
+				continue
 
 if __name__ == "__main__":
-    t0 = time.time()
-    for c in range(1, 31):
-        ww = link+str(c)
-        print "Downloading %s" % ww
-        try:
-            get_data(ww)
-            df = df.append(tmp, ignore_index=True)
-            time.sleep(1)
-        except Exception as e:
-            with open('error.log', 'a') as f:
-                f.write(str(ww)+'\n'+str(e)+'\n')
-                print e
-            continue
-    t_end = time.time()
-    print "Total elapsed time: %s seconds" % str(int(t_end - t0))
+	scraper = Scrape_User_Reviews(link, database, number_of_pages)
+	t_start = time.time()
+	scraper.Create_DB_and_Tables(database)
+	scraper.Run()
+	t_end = time.time()
+	print "Total time elapsed: %s seconds" % str(int(t_end - t_start))
